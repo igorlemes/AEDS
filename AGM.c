1e93da61db1773fe8452152eee5f26812e226d22
Igor <igorlemes@dcc.ufmg.br>
2018-11-07 02:50:59 -0200

    Updated

--- AGM.c
+++ AGM.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct node *link;
 struct graph {
     int numberOfVertex;
@@ -15,11 +14,21 @@ struct node{
 	int wheight;
 	link next;
 };
+void vectorInit(int *binaryVector, int size){
+	for (int i = 0; i < size; ++i)
+		binaryVector[i] = 0;
+}
+Graph minimumSpanningTree(Graph G){
+	Graph A = graphInit(G->numberOfVertex);
+	
+	int added = 0;
+	int binaryVector[G->numberOfVertex];
+	vectorInit(binaryVector, G->numberOfVertex);
 
-// Graph ArvoreGeradoraMinima(grafo G){
-   // grafo *A = malloc(sizeof(grafo) + G.tamanho);
-   // A.
-// }
+	while(added != G->numberOfVertex){
+
+	}   
+}
 
 /* Recebe um valor inteiro e cria um Grafo do tamanho deste inteiro.
 Retorna um ponteiro do Grafo criado */
@@ -33,17 +42,25 @@ Graph graphInit(int size){
     }
     return A;
 }
-link newNode(int vertexV, int wheightW, link next){
+link newNode(int vertexW, int wheightW, link next){
 	link node = malloc(sizeof(struct node));
 	node->vertex = vertexW;
 	node->wheight = wheightW;
 	node->next = next;
 	return node;
 }
-void insertEdge(Graph A, int vertexV, int vertexW){
-	for (int i = 0; i < count; ++i){
-		
-	}
+
+void insertEdge(Graph A, int vertexV, int vertexW, int wheightW){
+	if(isEdge(vertexV, vertexW)) return;
+	A->adjacent[vertexV] = newNode(vertexW, wheightW, A->adjacent[vertexV]);
+	A->adjacent[vertexW] = newNode(vertexV, wheightW, A->adjacent[vertexW]);
+	A->numberOfEdges++;
+}
+
+int isEdge(int vertexV, vertexW){
+	for (link a  = A->adjacent[vertexV]; a != NULL; a = a->next)
+		if(a->vertex == vertexW) return 1;
+	return 0;
 }
 
 // void insertFirst(Graph A, int vertex, ){
@@ -67,8 +84,23 @@ void arestaMenorPeso(grafo *A, int vertice){
 	}
 }
 
+int hasEdge(){
+
+}
+
 void imprimeAresta(){}
 
-int removeMeio(){}
+Graph graphIn(char *fileAddress){
+	FILE *fp;
+	fp = fopen(fileAddress, "r");
+	if(fp == NULL)
+		perror("The file does not have been passed\n");
+		return -1;
+	
+	char str[9];
+	
 
-void organizarLista(){}
+	fgets(line, )
+	while(fgets(line, ))
+	
+} 
