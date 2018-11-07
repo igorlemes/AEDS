da00bb662d31c900878ea79a992f6dd4d53e4dde
Igor <igorlemes@dcc.ufmg.br>
2018-11-07 00:10:52 -0200

    Atualizado

--- AGM.c
+++ AGM.c
@@ -1,32 +1,32 @@
-#include "AGM.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct lista{
-    int Aresta;
-    LISTA *proximo;
-} LISTA;
-
-typedef struct grafo {
-    int tamanho;
-    LISTA Vertices[];
-} GRAFO;
-
-GRAFO ArvoreGeradoraMinima(GRAFO G){
-    GRAFO *A = malloc(sizeof(GRAFO) + G.tamanho);
-    A.
+grafo ArvoreGeradoraMinima(grafo G){
+//    grafo *A = malloc(sizeof(grafo) + G.tamanho);
+//    A.
 }
 
 /* Recebe um valor inteiro e cria um Grafo do tamanho deste inteiro.
-Retorna o endereço do Grafo criado */
-GRAFO* CriaGrafo(int tamanho){
-    GRAFO *A = malloc(sizeof(GRAFO) + sizeof(int)*tamanho);
-    for(size_t i = 0; i < tamanho; i++){
-        (*A).Vertices[i].proximo = NULL;
+Retorna um ponteiro do Grafo criado */
+grafo *CriaGrafo(int tamanho){
+    grafo *A = malloc(sizeof(grafo));
+    A->tamanhoGrafo = tamanho;
+    A->vertices = malloc(A->tamanhoGrafo*sizeof(lista*));
+    int i;
+    for(i = 0; i < tamanho; i++){
+        A->vertices[i] = listaVazia();
     }
-    return &A;
+    return A;
 }
-void IncluirVertice(){
 
-}
-void ArestaMenorPeso(){
+void incluirInicio(grafo *A){}
 
-}
+void arestaMenorPeso(){}
+
+void imprimeAresta(){}
+
+int removeMeio(){}
+
+void listaVazia(){}
+
+void organizarLista(){}
--- AGM.h
+++ AGM.h
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-GRAFO ArvoreGeradoraMinima(GRAFO G);
+typedef struct lista{
+    int Aresta;
+    int Vertice;
+    struct lista *proximo;
+} lista;
+
+typedef struct {
+    int tamanhoGrafo;
+    lista **Vertices;
+} grafo;
+
+
+grafo ArvoreGeradoraMinima(grafo G);
